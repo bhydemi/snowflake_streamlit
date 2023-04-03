254f6bc34e25367fcf37f85d939c8df3377cf091
Abdulhakeem Omotolani Yaqoob <40827896+bhydemi@users.noreply.github.com>
2023-04-04 00:19:24 +0200

    streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,16 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvice Fruit Advice')
+import requests
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/kiwi')
+streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
+
