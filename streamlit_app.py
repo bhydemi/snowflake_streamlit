dcc694b1c0a0c1fc322d310a2f41d876332b06a2
Abdulhakeem Omotolani Yaqoob <40827896+bhydemi@users.noreply.github.com>
2023-04-03 08:32:01 +0200

    streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit 
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
