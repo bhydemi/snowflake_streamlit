d1a3551f4fff69b4770abc6333c59ee82b22eca0
Abdulhakeem Omotolani Yaqoob <40827896+bhydemi@users.noreply.github.com>
2023-04-04 02:14:01 +0200

    snowflake streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit 
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -23,6 +24,7 @@ streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
