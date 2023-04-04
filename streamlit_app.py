d0c8d8c4e4ac4253c3142f0b7671f324a349d1a4
Abdulhakeem Omotolani Yaqoob <40827896+bhydemi@users.noreply.github.com>
2023-04-04 02:20:29 +0200

    snowflake streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-streamlit.stop()
+
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
